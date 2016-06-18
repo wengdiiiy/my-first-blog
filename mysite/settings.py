e88c62e0b0d162d603596ce0c8a2c9167c9c84c8
Jeongmin <sksmswendy@naver.com>
2016-06-18 12:36:54 +0900

    [심화 튜토리얼] 오류 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '<FIXME>',
-        'USER': '<FIXME>',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
